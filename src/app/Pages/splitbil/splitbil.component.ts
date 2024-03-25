7a27215e7097f5664de2b84285517cdd69da3b43
irsyad <muirsyad2399@gmail.com>
2024-03-25 09:04:32 +0800

    push progress calc

--- src/app/Pages/splitbil/splitbil.component.ts
+++ src/app/Pages/splitbil/splitbil.component.ts
@@ -6,6 +6,8 @@ import { NgFor,NgClass } from '@angular/common';
 import { GlobalButtonComponent } from '../../global-button/global-button.component';
 import { BackdropComponent } from '../../Components/backdrop/backdrop.component';
 import { elementAt } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-splitbil',
@@ -15,11 +17,14 @@ import { elementAt } from 'rxjs';
   styleUrl: './splitbil.component.css'
 })
 export class SplitbilComponent implements OnInit {
+  
   total:string=""
   itemList!:Item[]
   paxlist!:string[]
+  paxitem:number[][]=[]
   backdrop:boolean = false
   modal:boolean[]=[]
+  npaid:number[]=[]
   ngOnInit() {
     var x = localStorage.getItem("item")
     var y = localStorage.getItem("pax")
@@ -55,4 +60,69 @@ export class SplitbilComponent implements OnInit {
     this.backdrop= !this.backdrop
   }
   
+  // chk(itemid:number, paxid:number){
+  //   // this.itemList[itemid].paid=[this.paxlist[paxid],"SS"]
+  //   if( this.itemList[itemid].paid != undefined){
+  //     if(this.itemList[itemid].paid!.includes(this.paxlist[paxid])){
+  //       var index = this.itemList[itemid].paid!.indexOf(this.paxlist[paxid]);
+  //       this.itemList[itemid].paid!.splice(index, 1);
+        
+  //     }else{
+  //       this.itemList[itemid].paid!.push(this.paxlist[paxid])
+  //     }
+      
+  //   }else{
+  //     this.itemList[itemid].paid=[this.paxlist[paxid]]
+  //   }
+
+  //   console.log(this.paxlist[paxid])
+  //   console.log(this.itemList[itemid])
+  // }
+
+  chk2(itemid:number, paxid:number){
+    // this.itemList[itemid].paid=[this.paxlist[paxid],"SS"]
+    if( this.itemList[itemid].paid != undefined){
+      if(this.itemList[itemid].paid!.includes(paxid)){
+        var index = this.itemList[itemid].paid!.indexOf(paxid);
+        this.itemList[itemid].paid!.splice(index, 1);
+        
+      }else{
+        this.itemList[itemid].paid!.push(paxid)
+      }
+      
+    }else{
+      this.itemList[itemid].paid=[paxid]
+    }
+
+    console.log(this.paxlist[paxid])
+    console.log(this.itemList[itemid])
+  }
+  calc(){
+    this.itemList.forEach((element,j)=>{
+      if(element.paid != undefined){
+        console.log(element.paid)
+        console.log(element.paid.length)
+        var nedpaid:number = element.price/element.paid.length
+        console.log("Need to paid RM"+ nedpaid)
+        this.npaid.push(nedpaid)
+        this.paxlist.forEach((el,ind)=>{
+          if(element.paid!.includes(ind) && j < 1){
+            this.paxitem.push([j])
+            console.log(j)
+          }else{
+            // this.paxitem[j].push(j)
+            console.log(j)
+            console.log(ind)
+
+            
+          }
+          console.log(this.paxitem)
+          // console.log(element.paid!.includes(ind))
+        })
+      }else{
+        this.npaid.push(0)
+      }
+    })
+    console.log(this.npaid)
+  }
 }
--- src/app/model/item.ts
+++ src/app/model/item.ts
@@ -2,5 +2,7 @@ export interface Item {
     item: string
     quantity:number
     price:number
+    // paid?:string[]
+    paid?:number[]
     
 }
